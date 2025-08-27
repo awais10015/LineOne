fff7c268003ce45b6a7f88a3deffca23977a3536
awais10015 <awais10015@gmail.com>
2025-08-27 10:10:53 +0500

    feed fixed

--- src/app/api/allposts/route.js
+++ src/app/api/allposts/route.js
@@ -1,6 +1,7 @@
 import { connect } from "@/lib/db";
 import { Post } from "@/models/postModel"; // adjust import path
 import { NextResponse } from "next/server";
+import {User} from "@/models/userModel"
 
 export async function GET() {
   try {
@@ -10,7 +11,9 @@ export async function GET() {
       .populate("taggedUsers")
       .sort({ createdAt: -1 }); // newest first
 
-    return NextResponse.json({success: true , posts });
+    const user = await User.findById("689d7a1e50f0ec15a52d131e")
+    
+    return NextResponse.json({success: true , user , posts });
   } catch (error) {
     console.error(error);
     return NextResponse.json(
